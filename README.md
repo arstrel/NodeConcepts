d37fb278b5e1e69e6e7127197bb62ee2cf899c30
Artem Streltov <arstrel@gmail.com>
2020-07-06 15:55:15 -0400

    It's looking good so far, gotta commit to GitHub

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advanced Node Concepts
+Example node - express - react app to learn and showcase some node concepts such as 
+- Authentication with passport and Google
+- Connecting to remote DB
+--- I'm here now ---
+- Data caching with Redis
+- Automated Headless Browser Testing
+- CI
+- Image/File upload
---
+++ config/dev.example.js
@@ -0,0 +1,8 @@
+// add proper credentials
+module.exports = {
+    googleClientID:
+      'i355ogsc.apps.googleusercontent.com',
+    googleClientSecret: 'qYK91fc-Ew',
+    mongoURI: 'mongodb://<username>:<password>@cluster0-shard-00-00-7pwfe.mongodb.net:27017,cluster0-shard-00-01-7pwfe.mongodb.net:27017,cluster0-shard-00-02-7pwfe.mongodb.net:27017/<DB name>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    cookieKey: '123123',
+  };
