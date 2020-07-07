eb6d236ab6c4d8442d02e89ba9acb1dbddadd19d
Artem Streltov <arstrel@gmail.com>
2020-07-06 21:22:28 -0400

    Added redis for just Blogs route

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Advanced Node Concepts
-Example node - express - react app to learn and showcase some node concepts such as 
+Example node - express - react - redux app to learn and showcase some node concepts such as 
 - Authentication with passport and Google
 - Connecting to remote DB
---- I'm here now ---
+- --- I'm here now ---
 - Data caching with Redis
 - Automated Headless Browser Testing
 - CI
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,29 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    // make client.get return promise instead of passing it a callback
+    client.get = util.promisify(client.get);
+
+    // Do we have any cached data in redis related to this query
+    const cachedBlogs = await client.get(req.user.id);
+
+    // if yes, then respond to the request right away and return
+    if(cachedBlogs) {
+      console.log('SERVING FROM CACHE')
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    // in no, then we need to respond to request and update
+    // our cache to store the data
     const blogs = await Blog.find({ _user: req.user.id });
 
+    console.log('SERVING from MongoDB');
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
